d89fb15350eca9d0f424672d00dca34fd066b229
Caio Petrucci Rosa <caiopetruccirosa@gmail.com>
2023-06-19 18:35:46 -0300

    adiciona solucao para o problema C do contest 02-06-2023

--- contest-2023-06-02/C.cpp
+++ contest-2023-06-02/C.cpp
@@ -7,53 +7,75 @@
 
 using namespace std;
 
-vector<int> dijkstra(vector<vector<pii>> &G, int c, int start) {
-    vector<pii> distance(G.size()+1, pii(INT_MAX, INT_MAX));
-    vector<bool> visited(G.size()+1, false);
+int dijkstra(vector<vector<pii>> &G, vector<int> &p, int c, int s, int e) {
+    vector<vector<int>> cost(G.size(), vector<int>(c+1, INT_MAX));
+    vector<vector<bool>> visited(G.size(), vector<bool>(c+1, false));
     priority_queue<pipii, vector<pipii>, greater<pipii>> pq;
 
-    distance[start] = 0;
-    pq.push(pipii(0, pii(0, start)));
+    cost[s][0] = 0;
+    pq.push(pipii(0, pii(0, s)));
 
     while (!pq.empty()) {
-        pii p = pq.top();
+        pipii v = pq.top();
         pq.pop();
 
-        if (!visited[p.second]) {
-            visited[p.second] = true;
-            for (auto adj : G[p.second]) {
-                if (!visited[adj.second]) {
-                    int d = distance[p.second] + adj.first;
-                    if (d < distance[adj.second]) {
-                        distance[adj.second] = d;
-                        pq.push(make_pair(d, adj.second));
-                    }
+        int current_cost = v.first;
+        int fuel = v.second.first;
+        int city = v.second.second;
+
+        if (!visited[city][fuel]) {
+            visited[city][fuel] = true;
+
+            for (pii neighbour : G[city]) {
+                int nei_distance = neighbour.first;
+                int nei_city = neighbour.second;
+                if (nei_distance <= fuel && 
+                    !visited[nei_city][fuel-nei_distance] &&
+                    cost[nei_city][fuel-nei_distance] > current_cost) {
+                    cost[nei_city][fuel-nei_distance] = current_cost;
+                    pq.push(pipii(current_cost, pii(fuel - nei_distance, nei_city)));
                 }
+            }
+
+            if (fuel+1 <= c && 
+                !visited[city][fuel+1] &&
+                cost[city][fuel+1] > current_cost+p[city]) {
+                cost[city][fuel+1] = current_cost + p[city];
+                pq.push(pipii(current_cost + p[city], pii(fuel + 1, city)));
             }
         }
     }
 
-    return distance;
+    return cost[e][0];
 }
 
 int main() {_
     int n, m;
     cin >> n >> m;
 
-    vector<vector<pii>> G(n+1);
+    vector<int> p(n);
+    for (int i = 0; i < n; i++)
+        cin >> p[i];
+
+    vector<vector<pii>> G(n);
     for (int i = 0; i < m; i++) {
         int a, b, w;
         cin >> a >> b >> w;
+        G[a].push_back(pii(w, b));
         G[b].push_back(pii(w, a));
     }
 
-    vector<int> distances = dijkstra(G, E);
-    int mouse = 0;
-    for (int d: distances)
-        if (d <= T)
-            mouse++;
-
-    cout << mouse << '\n';
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int c, s, e;
+        cin >> c >> s >> e;
+        int cost = dijkstra(G, p, c, s, e);
+        if (cost < INT_MAX)
+            cout << cost << '\n';
+        else
+            cout << "impossible\n";
+    }
 
     return 0;
 }
